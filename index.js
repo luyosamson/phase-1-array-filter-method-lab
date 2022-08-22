cece48f4bab01f384c38c421d8ac8aa24fcdf9fe
luyosamson <luyosamson@gmail.com>
2022-08-22 06:49:30 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers, stringText) {
+  return drivers.filter((possibleMatch) => possibleMatch.toLowerCase() === stringText.toLowerCase());
+}
+
+//returns names of drivers with provided letters
+function fuzzyMatch(drivers, stringWithLetters) {
+  return drivers.filter((possibleMatch) => possibleMatch.toLowerCase().indexOf(stringWithLetters.toLowerCase()) === 0);
+}
+
+//return each element whose name property matches the provided string argument.
+function matchName(drivers, driverName) {
+  return drivers.filter((record) => record.name === driverName);
+}
